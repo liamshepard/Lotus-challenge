9e092fe603c835ba2f227f0f5d1195822d414307
lsshepard <liam.robert.shepard@gmail.com>
2023-02-24 12:38:56 +0100

    added CubicBezier class

---
+++ CubicBezier.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class CubicBezier {
+
+    A;
+    B;
+    C;
+    D;
+
+    constructor(A, B, C, D) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
+
+    distance(P) {
+        
+    }
+}
